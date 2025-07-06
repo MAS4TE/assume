5bd8eb29d264a60388c99bc58f1bff0308782ab4
ChriKo <komanns@fh-aachen.de>
2025-07-06 13:53:59 +0200

    basic workflow of optimization framework in strategy

--- mas4te/mas4te_bidding_strategy.py
+++ mas4te/mas4te_bidding_strategy.py
@@ -1,18 +1,75 @@
-from assume.common.base import BaseStrategy, SupportsMinMax, SupportsMinMaxCharge
+from assume.common.base import BaseStrategy, SupportsMinMaxCharge
 from assume.common.market_objects import MarketConfig, Orderbook, Product
 import requests
+import pandas as pd
+from pricing_framework import Storage, Optimizer
 
-class LLMStrategy(BaseStrategy):
-    """
-    A simple strategy where the user can input the bids of an agent manually.
-    Only one agent should have this strategy in a simulation in one process, so that the terminal does not wait for multiple responses.
+# TODO: implement
+def build_demand_timeseries(unit: SupportsMinMaxCharge) -> pd.DataFrame:
+    """Builds a demand timeseries for the given unit.
 
-    Methods
-    -------
+    Returns:
+        pd.DataFrame: The demand timeseries for the unit.
     """
-    def __init__(self, api_url="http://localhost:1234/v1/completions", *args, **kwargs ):
+    pass
+
+# TODO: implement
+def build_price_timeseries(unit: SupportsMinMaxCharge) -> pd.DataFrame:
+    """Builds a price timeseries for the given unit.
+
+    Args:
+        unit (SupportsMinMaxCharge): The unit to build the price timeseries for.
+
+    Returns:
+        pd.DataFrame: The price timeseries for the unit.
+    """
+    pass
+
+# TODO: implement
+def build_solar_generation_timeseries(unit: SupportsMinMaxCharge) -> pd.DataFrame:
+    """Builds a solar generation timeseries for the given unit.
+
+    Args:
+        unit (SupportsMinMaxCharge): The unit to build the solar generation timeseries for.
+
+    Returns:
+        pd.DataFrame: The solar generation timeseries for the unit.
+    """
+    pass
+
+# TODO: implement
+def build_storages_to_calculate() -> list[Storage]:
+    """Builds a list of storages to calculate for.
+
+    Returns:
+        list[Storage]: The list of storages to calculate for.
+    """
+    # This is a placeholder implementation. Replace with actual logic to build storages.
+    return [Storage(id=str(i), volume=i, c_rate=1, efficiency=0.9) for i in range(1, 10)]
+
+# TODO: implement
+def get_baseline_storage(unit: SupportsMinMaxCharge) -> Storage:
+    """Gets the baseline storage for the given unit.
+
+    Args:
+        unit (SupportsMinMaxCharge): The unit to get the baseline storage for.
+
+    Returns:
+        Storage: The baseline storage for the unit.
+    """
+    # This is a placeholder implementation. Replace with actual logic to get baseline storage.
+    return Storage(id="baseline", volume=0, c_rate=1, efficiency=0.9)
+
+class LLMBuyStrategy(BaseStrategy):
+    """
+    A strategy that uses a Large Language Model (LLM) for a storage buyer.
+
+    Params:
+        llm_api_url (str): The URL of the LLM API to use for generating bids.
+    """
+    def __init__(self, llm_api_url, *args, **kwargs ):
         super().__init__()
-        self.api_url = api_url
+        self.api_url = llm_api_url
         self.headers = {"Content-Type": "application/json"}
 
     def run_prompt(self, prompt: str, model="Mistral-7B-Instruct-v0.3-Q4_K_M", max_tokens=1000):
@@ -28,96 +85,66 @@ class LLMStrategy(BaseStrategy):
 
     def calculate_bids(
         self,
-        unit: SupportsMinMax,
+        unit: SupportsMinMaxCharge,
         market_config: MarketConfig,
         product_tuples: list[Product],
         **kwargs,
     ) -> Orderbook:
-        """
-        Takes information from a unit that the unit operator manages and
-        defines how it is dispatched to the market.
+        """Calculates the value of multiple storage volumes for a predicted demand and price timeseries via linear optimization.
 
         Args:
-            unit (SupportsMinMax): The unit to be dispatched.
-            market_config (MarketConfig): The market configuration.
-            product_tuples (list[Product]): The list of all products the unit can offer.
+            unit (SupportsMinMaxCharge): The unit to calculate bids for.
+            market_config (MarketConfig): The market configuration to use.
+            product_tuples (list[Product]): The list of products to calculate bids for.
 
         Returns:
-            Orderbook: The bids consisting of the start time, end time, only hours, price and volume.
+            Orderbook: The calculated order book with bids.
         """
-        start = product_tuples[0][0]  # start time of the first product
-        end_all = product_tuples[-1][1]  # end time of the last product
-        previous_power = unit.get_output_before(
-            start
-        )  # power output of the unit before the start time of the first product
-        op_time = unit.get_operation_time(start)
-        min_power_values, max_power_values = unit.calculate_min_max_power(
-            start, end_all
-        )  # minimum and maximum power output of the unit between the start time of the first product and the end time of the last product
+        
+        # start of by reading in / generating the data for demand and costs
+        demand_timeseries = build_demand_timeseries(unit)
+        pricing_timeseries = build_price_timeseries(unit)
+        solar_generation = build_solar_generation_timeseries(unit)
 
-        bids = []
+        # build storages to optimize for
+        storages_to_calculate = build_storages_to_calculate()
 
-        # TODO create only a single llm query
-        for product, min_power, max_power in zip(
-            product_tuples, min_power_values, max_power_values
-        ):
-            # for each product, calculate the marginal cost of the unit at the start time of the product
-            # and the volume of the product. Dispatch the order to the market.
-            start = product[0]
-            current_power = unit.outputs["energy"].at[
-                start
-            ]  # power output of the unit at the start time of the current product
-            marginal_cost = unit.calculate_marginal_cost(
-                start, previous_power
-            )  # calculation of the marginal costs
-            volume = unit.calculate_ramp(
-                op_time, previous_power, max_power, current_power
-            )
-            print(f"> requesting bid for time from {product[0]} to {product[1]}")
-            print(f"> power must be between {min_power} and {max_power}")
-            print(f"> {previous_power=}, {current_power=}, {marginal_cost=}")
-            try:
-                # prompt = input(
-                #     "> waiting for volume and price, space-separated with a dot as decimal point \n"
-                # )
-                # volume, price = prompt.split(" ")
-                # volume, price = float(volume), float(price)
-                #price_forecast = unit.forecaster.get_price(unit.fuel_type)
-                price_forecast = unit.forecaster.get_price("demand")
-                price = 0
-                volume = 0
+        # baseline storage volume
+        baseline_storage = get_baseline_storage(unit)
 
-                # prompt = "create prices and volumes for following products: {product_tuples} with the forecast: {price_forecast}"
+        # get baseline optimization, to know how much prosumer has to pay with current setup
+        optimizer = Optimizer(
+            storage=baseline_storage,
+            prices=pricing_timeseries,
+            solar_generation=solar_generation,
+            demand=demand_timeseries,
+            storage_use_cases=["eeg", "wholesale", "community", "home"],
+        )
+        optimizer.optimize(solver="gurobi")
+        baseline_cost = optimizer.model.objective()
 
-                # TODO bea get response from LLM
-                # volume, price = run_prompt( ... )
-            except ValueError:
-                volume, price = 0, 0
+        # dictionary to hold the worth of each storage in
+        # with volume as key and worth as value
+        storages_worth = {}
 
-            bids.append(
-                {
-                    "start_time": product[0],
-                    "end_time": product[1],
-                    "only_hours": product[2],
-                    "price": price,
-                    "volume": volume,
-                    "node": unit.node,
-                }
+        for storage in storages_to_calculate:
+            # create the optimizer
+            optimizer = Optimizer(
+                storage=storage,
+                prices=pricing_timeseries,
+                solar_generation=solar_generation,
+                demand=demand_timeseries,
+                storage_use_cases=["eeg", "wholesale", "community", "home"],
             )
 
-            if "node" in market_config.additional_fields:
-                bids[-1]["max_power"] = unit.max_power if volume > 0 else unit.min_power
-                bids[-1]["min_power"] = (
-                    min_power[start] if volume > 0 else unit.max_power
-                )
+            # run the optimization
+            optimizer.optimize(solver="gurobi")
 
-            previous_power = volume + current_power
-            if previous_power > 0:
-                op_time = max(op_time, 0) + 1
-            else:
-                op_time = min(op_time, 0) - 1
+            # minimum cost in this scenario is the objective of the optimization model
+            minimum_cost = optimizer.model.objective()
 
-        if "node" in market_config.additional_fields:
-            return bids
-        else:
-            return self.remove_empty_bids(bids)
+            # worth of the storage is the difference between the baseline cost and the minimum cost
+            worth = minimum_cost - baseline_cost
+
+            # add to storage_worth dictionary
+            storages_worth[storage.volume] = worth
