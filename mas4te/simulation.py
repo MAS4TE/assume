ffca38353bfeabb15aea3488f45b4a6c2662b074
ChriKo97 <c.komanns@proton.me>
2025-10-31 11:13:49 +0100

    Bugfix randomness

--- mas4te/simulation.py
+++ mas4te/simulation.py
@@ -161,12 +161,12 @@ def init(
     n_supply_units = n_supply_units
     n_demand_units = n_demand_units
 
-    if n_supply_units > 50:
+    if int(n_supply_units) > 50:
         supply_profiles = list(range(n_supply_units))
     else:
         supply_profiles = np.random.randint(low=0, high=119, size=int(n_supply_units))
 
-    if n_demand_units > 50:
+    if int(n_demand_units) > 50:
         demand_profiles = list(range(n_demand_units))
     else:
         demand_profiles = np.random.randint(low=0, high=119, size=int(n_demand_units))
